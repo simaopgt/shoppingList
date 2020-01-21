a85229791d2fc740a3c311514b91fdd89c9788d4
spedro <spedro@thoughtworks.com>
2020-01-21 19:39:16 -0300

    Adding features to hide the keyboard and reset imput fields.

--- app/src/main/java/com/simao/shoppingList/view/MainActivity.kt
+++ app/src/main/java/com/simao/shoppingList/view/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.simao.shoppingList.view
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.InputMethodManager
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.RecyclerView
 import com.simao.shoppingList.R
@@ -10,10 +12,9 @@ import com.simao.shoppingList.view.adapter.ListAdapter
 import com.simao.shoppingList.viewmodel.ShoppingListViewModel
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_main.add_item_name_textView
-import kotlinx.android.synthetic.main.list_item.*
 import org.koin.android.viewmodel.ext.android.viewModel
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(){
 
     private val shoppingListViewModel : ShoppingListViewModel by viewModel()
 
@@ -40,6 +41,21 @@ class MainActivity : AppCompatActivity() {
         add_button.setOnClickListener{
             val itemsList = ItemsList(getItemName(), getItemQtd(), "False")
             shoppingListViewModel.insert(itemsList)
+            resetFields()
+            closeKeyboard()
+        }
+    }
+
+    private fun resetFields() {
+        add_item_name_textView.text.clear()
+        add_item_qtd_textView.text.clear()
+    }
+
+    private fun closeKeyboard() {
+        val view = this.currentFocus
+        view?.let { v ->
+            val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+            inputMethodManager?.hideSoftInputFromWindow(v.windowToken, 0)
         }
     }
 
